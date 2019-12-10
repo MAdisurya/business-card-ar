3659ec5205161384903f5dc81753b779f37c12ab
Mario Adisurya <>
2019-12-10 18:35:37 +1300

    * main.js
    * App

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ class App extends React.Component
                 {/* <a-box position='0 0.5 0' material='opacity: 0.7;'></a-box> */}
                 <Container 
                     initialPos={new Position(1, 0.5, 0)}
+                    initialRot={new Rotation(-90, 0, 0)}
                     initialSize={new Size(1, 1)}
                 />
                 <a-marker-camera preset='hiro'></a-marker-camera>
