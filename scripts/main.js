69deef256c54c086625da33f38cac38d0f36152a
Mario Adisurya <>
2019-12-11 23:27:22 +1300

    * index.html * main.js + Integrated ARContainer and ARText in App.render()

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,16 @@ class App extends React.Component
         return (
             <a-scene embedded>
                 {/* <a-box position='0 0.5 0' material='opacity: 0.7;'></a-box> */}
-                <Container 
+                <ARContainer 
                     initialPos={new Position(1, 0.5, 0)}
                     initialRot={new Rotation(-90, 0, 0)}
-                    initialSize={new Size(1, 1)}
-                />
+                    initialSize={new Size(1, 1)}>
+                        <ARText 
+                            value="Mario"
+                            size={new Size(1, 1)}
+                            align="center"
+                        />
+                </ARContainer>
                 <a-marker-camera preset='hiro'></a-marker-camera>
             </a-scene>
         );
