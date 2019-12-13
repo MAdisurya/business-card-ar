16fd4590d8951e68ef646cefefd1029b46d65110
Mario Adisurya <>
2019-12-13 15:40:55 +1300

    * main.js + assets/images
    + Integrated ARImageContainer in App for Profile image placeholder addition

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,26 @@ class App extends React.Component
     {
         return (
             <a-scene embedded>
+                {/* // Assets */}
+                <a-assets>
+                    <img 
+                        id="profile-img" 
+                        src="../assets/images/empty-profile.png" />
+                </a-assets>
+
                 {/* <a-box position='0 0.5 0' material='opacity: 0.7;'></a-box> */}
                 <ARContainer
                     initialPos={new Position(1.5, 0.5, 0)}
                     initialSize={new Size(3, 2)}>
                     {/* Replace this Container with ARVideoContainer when created */}
                 </ARContainer>
+                <ARImageContainer
+                    initialPos={new Position(4.5, 0.5, 0)}
+                    initialSize={new Size(2, 2)}
+                    imageSource="#profile-img">
+                </ARImageContainer>
                 <ARContainer 
-                    initialPos={new Position(5, 0.5, 0)}
+                    initialPos={new Position(7, 0.5, 0)}
                     initialSize={new Size(2, 0.5)}>
                         <ARHeaderText 
                             value="Mario"
@@ -19,7 +31,7 @@ class App extends React.Component
                         />
                 </ARContainer>
                 <ARContainer
-                    initialPos={new Position(5, 0.5, 0.75)}
+                    initialPos={new Position(7, 0.5, 0.75)}
                     initialSize={new Size(2, 0.5)}>
                         <ARHeaderText
                             value="Adisurya"
