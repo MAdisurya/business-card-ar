bb4ac33fcd25cc3e7694f10b1b3fd876e5ac5ecc
Mario Adisurya <>
2019-12-18 12:12:46 +1300

    * main.js
    + Added asset phone-icon.png
    + Added asset mail-icon.png
    
    + Implemented Contact Section with ARIconContainers & ARContainers

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,20 @@ class App extends React.Component
     {
         return (
             <a-scene embedded>
-                {/* // Assets */}
+                {/* Assets */}
                 <a-assets>
+                    {/* Profile Image */}
                     <img 
                         id="profile-img" 
                         src="../assets/images/empty-profile.png" />
+                    
+                    {/* Contact Images */}
+                    <img
+                        id="phone-img"
+                        src="../assets/images/phone-icon.png" />
+                    <img
+                        id="email-img"
+                        src="../assets/images/mail-icon.png" />
                 </a-assets>
 
                 {/* <a-box position='0 0.5 0' material='opacity: 0.7;'></a-box> */}
@@ -60,6 +69,36 @@ class App extends React.Component
                     initialAnchor={new Position(0.5, 0.5, 0)}
                     initialSize={new Size(3, 2)}>
 
+                </ARContainer>
+
+                {/* Contact Section */}
+                <ARIconContainer
+                    initialPos={new Position(3, 0.5, 1.5)}
+                    initialAnchor={new Position(1, 0.5, 0)}
+                    imageSource="#phone-img">
+                </ARIconContainer>
+                <ARContainer 
+                    initialPos={new Position(3, 0.5, 1.75)}
+                    initialAnchor={new Position(0, 0.5, 0)}
+                    initialSize={new Size(4, 0.5)}>
+                        <ARHeaderText 
+                            value="+64 21 024 54709"
+                            size={new Size(4, 1)}
+                        />
+                </ARContainer>
+                <ARIconContainer
+                    initialPos={new Position(3, 0.5, 2.5)}
+                    initialAnchor={new Position(1, 0.5, 0)}
+                    imageSource="#email-img">
+                </ARIconContainer>
+                <ARContainer 
+                    initialPos={new Position(3, 0.5, 2.75)}
+                    initialAnchor={new Position(0, 0.5, 0)}
+                    initialSize={new Size(5, 0.5)}>
+                        <ARHeaderText 
+                            value="mario.adisurya@gmail.com"
+                            size={new Size(5, 1)}
+                        />
                 </ARContainer>
 
                 <a-marker-camera preset='hiro'></a-marker-camera>
